bf661ab4bab3beebcd00dfb70a2aeac43a4d5fd3
angelkaxxx888-glitch <angelkaxxx888@gmail.com>
2026-04-05 17:10:54 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDZkrtOXISbUO3Fb_zvsyzzH1aS4ee8ZTI",
+  authDomain: "bridgetoher.firebaseapp.com",
+  projectId: "bridgetoher",
+  storageBucket: "bridgetoher.firebasestorage.app",
+  messagingSenderId: "1082688277555",
+  appId: "1:1082688277555:web:72328598b456fe0aade746"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
